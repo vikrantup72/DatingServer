951aacf3fc043880640da68eb6556536756089c4
vikrantindigg <vikrant.upadhyay@indi.gg>
2023-12-30 10:43:14 +0530

    minor changes

--- routes/UploadRoute.js
+++ routes/UploadRoute.js
@@ -1,25 +1,42 @@
-import express from 'express'
-const router = express.Router()
-import multer from 'multer'
+import express from "express";
+import mongoose from "mongoose";
+import multer from "multer";
+
+const router = express.Router();
+
+const imageSchema = new mongoose.Schema({
+	name: String,
+	path: String,
+});
+
+const Image = mongoose.model("Image", imageSchema);
 
 const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, "public/images");
-    },
-    filename: (req, file, cb) => {
-      cb(null, req.body.name);
-    },
-  });
+	destination: (req, file, cb) => {
+		cb(null, "public/images");
+	},
+	filename: (req, file, cb) => {
+		cb(null, req.body.name);
+	},
+});
+
 const upload = multer({ storage: storage });
 
+router.post("/", upload.single("file"), async (req, res) => {
+	try {
+		// Save image information to MongoDB
+		const newImage = new Image({
+			name: req.body.name,
+			path: req.file.path,
+		});
 
-router.post("/", upload.single("file"), (req, res) => {
-    try {
-      return res.status(200).json("File uploded successfully");
-    } catch (error) {
-      console.error(error);
-    }
-  });
+		await newImage.save();
 
-export default router
+		return res.status(200).json("File uploaded successfully");
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json("Internal Server Error");
+	}
+});
 
+export default router;
